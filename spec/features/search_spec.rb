4d22f22eb9e6f30a74920e2c499d316ab87eeb59
ivetas <iveta.sandere@yahoo.com>
2015-05-28 14:09:31 +0300

    search tests

---
+++ spec/features/search_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+feature "Search" do
+  let!(:receipt)  { create :receipt }
+  let!(:category) { create :category }
+
+  before do
+    visit receipts_path
+  end
+
+  it "All users can search receipts in all receipts" do
+    fill_in "search", with: "Sk"
+
+    click_button I18n.t('search')
+
+    expect(page).to have_content receipt.title
+  end
+
+  it "All users can search receipts in specific category" do
+    receipt.categories << category
+
+    click_link category.name
+
+    fill_in "search", with: "karote"
+
+    click_button I18n.t('search')
+
+    expect(page).to have_content receipt.title
+  end
+end
